e74301dfa98b1a8d8d017a6a69547b250a7ad990
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2023-07-08 00:29:11 +0000

    Ajout du dossier répertoire avec le fichier Répertoire.java

---
+++ Repertoirepackage/Repertoire.java
@@ -0,0 +1,38 @@
+package Repertoirepackage;
+
+public class Repertoire {
+    private String nom; //stocke nom du répertoire
+    private Object[] elements; //stocke les éléments (répertoires ou fichiers)
+    private int nbElements; //stocke le nombre d'éléments dans le répertoire
+
+    public Repertoire(String nom) {
+        this.nom = nom;
+        this.elements = new Object[10]; // Par exemple, initialisation avec une taille de 10
+        this.nbElements = 0;
+    }
+
+    public void ajoutElement(Object element) { //cette méthode ajoute un ou des (répertoire ou fichier) au répertoire courant 
+        elements[nbElements] = element;
+        nbElements++;
+    }
+
+    public void afficherElement() { //cette méthode affiche les éléments du répertoire courant en faisant appelle à une méthode récursive
+        afficherElementRecursive(this, 0);
+    }
+
+    private void afficherElementRecursive(Repertoire repertoire, int profondeur) {
+        String indent = "│   ".repeat(profondeur);
+        String pre = indent + "├───";
+
+        System.out.println(pre + repertoire.nom);
+
+        for (int i = 0; i < repertoire.nbElements; i++) {
+            Object element = repertoire.elements[i];
+            if (element instanceof Repertoire) {
+                afficherElementRecursive((Repertoire) element, profondeur + 1);
+            } else if (element instanceof Fichier) {
+                System.out.println(indent + "│   ├───" + ((Fichier) element).getNom());
+            }
+        }
+    }
+}
