549bd47efe34b212c5bfa4111960db0c0ed3b26c
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2023-07-09 16:35:11 +0000

    Ajout du fichier Fichier.java

---
+++ Repertoirepackage/Fichier.java
@@ -0,0 +1,13 @@
+package Repertoirepackage;
+
+public class Fichier {
+    private String nom;
+
+    public Fichier(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
